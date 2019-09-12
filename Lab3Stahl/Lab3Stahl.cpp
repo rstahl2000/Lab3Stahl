b7e57fad4335708fdef1735d0f395d46374b3fb9
Robert Stahl <rstahl@udel.edu>
2019-09-12 14:45:23 -0400

    Adds Quiz Class

--- Lab3Stahl/Lab3Stahl.cpp
+++ Lab3Stahl/Lab3Stahl.cpp
@@ -8,7 +8,45 @@
 
 #include <iostream>
 using namespace std;
+#include <time.h>
+#include <stdlib.h>
+
+class Quiz{
+	int quizNum;
+	int qRight;
+	int totQuiz;
+public:
+	Quiz(int num){
+		quizNum=num;
+		qRight=0;
+		totQuiz=0;
+		srand(time(NULL));
+	}
+	void quizUser(){
+		int test=rand()%13+1;
+		int userAnswer;
+		cout<<"What is "<<test<<" * "<<quizNum<<"?"<<endl;
+		cin>>userAnswer;
+		if (userAnswer==(test*quizNum)){
+			qRight++;
+			cout<<"Correct!"<<endl;
+		}
+		else{
+			cout<<"Incorrect! Answer Was "<<(test*quizNum)<<endl;
+		}
+		totQuiz++;
+
+	}
+	void printScore(){
+		cout<<"You have "<<qRight<<" correct out of "<<totQuiz<<endl;
+	}
+};
 
 int main(){
+	Quiz test(6);
+	for(int i=0;i<=10;i++){
+		test.quizUser();
+		test.printScore();
+	}
 	return 0;
 }
